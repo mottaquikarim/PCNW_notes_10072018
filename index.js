70487bc669bb5691c086277a3a96c938bfb5900b
Mottaqui Karim <taqqui.karim@gmail.com>
2018-10-07 11:31:21 -0400

    initial madlibs implementation, it sucks rn tho

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ myAge++;
                 multilined
                     comment
 */
+
+const noun1 = "heart"
+const noun2 = "friends"
+const noun3 = "pet"
+
+const madlib = `Be kind to your ${noun1}-footed ${noun2}
+For a duck may be somebody\'s ${noun3},`;
+
+
+console.log(madlib)
+
+const noun11 = "liver"
+const noun21 = "enemies"
+const noun31 = "roommate"
+
+const madlib1 = `Be kind to your ${noun11}-footed ${noun21}
+For a duck may be somebody\'s ${noun31},`;
+
+
+console.log(madlib1)
+
+
+
