0fa813fa3ec9b0a61fa4111f5a3aff9b205c6b89
Mottaqui Karim <taqqui.karim@gmail.com>
2018-10-07 10:32:37 -0400

    initial add of class notes, lol

---
+++ index.js
@@ -0,0 +1,17 @@
+const mySocialSecurityNumber = "XXX-XX-XXXX";
+let myAge = 28;
+
+myAge = myAge + 1;
+myAge += 1;
+myAge++;
+
+// putting_underscores_between is called snake casing
+// my_age
+
+/*
+    this
+        is 
+            a
+                multilined
+                    comment
+*/
